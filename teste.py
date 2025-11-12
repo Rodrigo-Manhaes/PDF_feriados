658287ed74730e237149ce01fe782562dbc5125a
Rodrigo-Manhaes <rmsv2007@gmail.com>
2025-11-12 10:35:01 -0300

    teste.py

--- teste.py
+++ teste.py
@@ -1,10 +1,18 @@
 import tkinter as tk
 from tkinter import filedialog
 
+def selecionar_arquivo():
+    caminho_do_arquivo = filedialog.askopenfilename(title = "Selecione um Arquivo")
 
 root = tk.Tk()
-root.withdraw()
-caminho_do_arquivo = filedialog.askopenfilename()
+root.title("Leitor de arquivo")
+botao = tk.Button(root, text="Selecionar Arquivo", command=selecionar_arquivo)
+botao.pack(padx=20, pady=20)
+
+root.mainloop()
+
+
+
 
 
 
