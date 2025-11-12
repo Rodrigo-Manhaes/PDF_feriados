2ad81340e13a37e62843efa6bc6677a8df5d9ec9
Rodrigo-Manhaes <rmsv2007@gmail.com>
2025-11-12 10:36:43 -0300

    Add files via upload

--- teste.py
+++ teste.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog
+import requests
 
 def selecionar_arquivo():
     caminho_do_arquivo = filedialog.askopenfilename(title = "Selecione um Arquivo")
@@ -9,10 +10,15 @@ root.title("Leitor de arquivo")
 botao = tk.Button(root, text="Selecionar Arquivo", command=selecionar_arquivo)
 botao.pack(padx=20, pady=20)
 
-root.mainloop()
+url = "https://date.nager.at/api/v3/PublicHolidays/2025/BR"
 
+payload = {}
+headers = {
+  'accept': 'application/json',
+  'X-CSRF-TOKEN': 'pYBqfz7tfH5NFeqA2YXNhdZIsqRCMmef6FjOTNJz'
+  }
 
+response = requests.request("GET", url, headers=headers, data=payload)
 
-
-
+print(response.text)
 
